15ad09ec35e9662fbc1ebab30aca71830bef3d1a
Mehul  Vig <mehulvig6@gmail.com>
2024-09-18 23:43:00 +0530

    Lab work 3 updated

--- Lab Work 3/Problem_08.c
+++ Lab Work 3/Problem_08.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-int main(){
-    
+int isPowerOfTwo(int num) {
+    // A number is a power of 2 if it has only one bit set to 1 in its binary form.
+    // This can be checked using the condition: num & (num - 1) == 0 and num > 0
+    return (num > 0) && ((num & (num - 1)) == 0);
+}
+
+int main() {
+    int num;
+
+    // Input number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check if the number is a power of 2
+    if (isPowerOfTwo(num)) {
+        printf("%d is a power of 2.\n", num);
+    } else {
+        printf("%d is not a power of 2.\n", num);
+    }
+
+    return 0;
 }
