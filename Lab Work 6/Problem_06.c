54dd4b698711a975ab8d5a0b2c3e5f9a84af9e3e
Mehul  Vig <mehulvig6@gmail.com>
2024-10-31 17:53:31 +0530

    Lab work 6 completed.

--- Lab Work 6/Problem_06.c
+++ Lab Work 6/Problem_06.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+int power(int base, int exp){
+    if(exp == 0){
+        return 1;
+    }
+
+    return base * power(base, exp - 1 );
+}
+int main() {
+    int base , exp;
+    int base;
+    printf("Enter the value of base: ");
+    scanf("%d", &base);
+    int exp;
+    printf("Enter the value of power: ");
+    scanf("%d", &exp);
+    printf("%d raised to the power of %d is %d\n", base, exp, power(base, exp));
+    return 0;
+}
